3aac25125ec9f64313655d5ab6136a2b166ce9f0
longjika@UTORONTO.CA <longjika@UTORONTO.CA@08fbd934-1aca-4df4-a1c4-682f1001a59e>
2017-06-16 23:35:03 +0000

    first step complete

--- A2/utils.c
+++ A2/utils.c
@@ -13,6 +13,24 @@
  * Return the number of tokens, and store pointers to them in cmd_argv.
  */
 int tokenize(char *cmd, char **cmd_argv) {
+	char *token;
+	int argc = 0;
+	//get the first token
+	token = strtok(cmd, " ");
+	//insert the first token ino the array
+	//walk through other tokens
+	while (token != NULL) {
+
+		cmd_argv[argc] = token;
+		// remove the "\n" from the last token 
+		if(cmd_argv[argc][strlen(cmd_argv[argc]) -1] == '\n'){
+			cmd_argv[argc][strlen(cmd_argv[argc]) -1] = '\0';
+		}
+				
+		token = strtok(NULL , " ");
+		argc++;
+		
+	}
         // returning 0 for now, so the compiler does not complain
 	return 0;
 }
